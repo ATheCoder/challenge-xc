89da550beada7726f838e58ee35b2566882810a4
Arash Arbabi <arbabiarash0@gmail.com>
2023-06-04 17:55:39 +0330

    tests: add tests for favorite routes

---
+++ tests/favorite.test.ts
@@ -0,0 +1,51 @@
+import request from "supertest";
+import { app } from "../src/api";
+import { mongoIdRegex } from "./utils";
+
+const agent = request(app);
+
+describe('Testing "Favority" functionality', () => {
+  it("should return the list of seed favorities", async () => {
+    const res = await agent.get("/api/favorite");
+
+    expect(res.status).toBe(200);
+    expect(Object.keys(res.body)).toStrictEqual(["favorite"]);
+    expect(res.body["favorite"]).toHaveLength(1);
+
+    expect(res.body["favorite"][0]).toMatchObject({
+      favorite1: "String",
+      favorite2: "String",
+      favorite3: "String",
+      name: "String",
+    });
+  });
+
+  describe("Should find a favorite for a user", () => {
+    let profileId: string;
+    it("should get the profileId of the seed user", async () => {
+      const res = await agent.get("/api/profile");
+
+      expect(res.body["profile"]).toHaveLength(1);
+
+      const profile = res.body.profile[0];
+
+      profileId = profile._id;
+
+      expect(profileId).toMatch(mongoIdRegex);
+    });
+
+    it("should find a favorite for the profileId", async () => {
+      const res = await agent.get(`/api/favorite/${profileId}`);
+
+      expect(res.body).toHaveLength(1);
+
+      expect(res.body[0]).toMatchObject({
+        favorite1: "String",
+        favorite2: "String",
+        favorite3: "String",
+        name: "String",
+        profile_id: profileId,
+      });
+    });
+  });
+});
---
+++ tests/utils.ts
@@ -0,0 +1,4 @@
+/**
+ * Regular expression matching a MongoDB `_id` field
+ */
+export const mongoIdRegex = /^[0-9a-fA-F]{24}$/;
