a669d412ceec674732be7f1a5e3efb857abd3e04
Arash Arbabi <arbabiarash0@gmail.com>
2023-06-04 19:36:20 +0330

    tests: add tests for simulator

---
+++ tests/simulator.test.ts
@@ -0,0 +1,70 @@
+import request from "supertest";
+import { app } from "../src/api";
+import { mongoIdRegex } from "./utils";
+
+const agent = request(app);
+
+describe('Tests for the "Simulator" functionality', () => {
+  it("should get the list of seed simulators", async () => {
+    const res = await agent.get("/api/simulator");
+
+    expect(Object.keys(res.body)).toStrictEqual(["simulator"]);
+
+    const { simulator: simulators } = res.body;
+
+    expect(simulators).toHaveLength(1);
+
+    expect(simulators[0]).toMatchObject({
+      cryptocurrency: "String",
+    });
+  });
+
+  describe("should get simulators of a single profile_id", () => {
+    let profileId: string;
+    // TODO: This can be converted to a function because it is also used inside the favorite.test.ts
+    it("should get the profileId of the seed user", async () => {
+      const res = await agent.get("/api/profile");
+
+      expect(res.body["profile"]).toHaveLength(1);
+
+      const profile = res.body.profile[0];
+
+      profileId = profile._id;
+
+      expect(profileId).toMatch(mongoIdRegex);
+    });
+    it("should get simulators of a single profile_id", async () => {
+      const res = await agent.get(`/api/simulator/${profileId}`);
+
+      expect(res.body).toHaveLength(1);
+
+      expect(res.body[0]).toMatchObject({
+        cryptocurrency: "String",
+        profile_id: profileId,
+      });
+    });
+
+    it("should create simulator for a profile_id", async () => {
+      const createSimulatorRes = await agent
+        .post(`/api/simulator/${profileId}`)
+        .send({
+          cryptocurrency: "eth",
+          euros: 100,
+          price: 27000,
+          quantity: 1,
+        });
+
+      expect(createSimulatorRes.status).toBe(200);
+
+      const getSimulatorsRes = await agent.get(`/api/simulator/${profileId}`);
+
+      expect(getSimulatorsRes.body).toHaveLength(2);
+
+      const createdSimulator = getSimulatorsRes.body.find(
+        (sim) => sim.cryptocurrency === "eth" && sim.price === 27000
+      );
+
+      expect(createdSimulator).toBeTruthy();
+    });
+  });
+});
